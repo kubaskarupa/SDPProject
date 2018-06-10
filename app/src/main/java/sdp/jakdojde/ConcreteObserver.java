7f2b065053ba639250d8500b442caa4003e0ab81
kubaskarupa <38496202+kubaskarupa@users.noreply.github.com>
2018-06-11 01:33:55 +0200

    update textView method added

--- app/src/main/java/sdp/jakdojde/ConcreteObserver.java
+++ app/src/main/java/sdp/jakdojde/ConcreteObserver.java
@@ -1,10 +1,19 @@
 package sdp.jakdojde;
 
+import android.widget.TextView;
+
 public class ConcreteObserver  implements Observer {
+    TextView textView = null;
     private String distance;
     @Override
     public void update(String distanceee) {
         distance = distanceee;
+        updateTextView(distance);
+    }
+    public void updateTextView(String toThis) {
+
+        textView = (TextView) textView.findViewById(R.id.textView2);
+        textView.setText(toThis);
     }
 
 }
