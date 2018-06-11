db22bf9dd1c6294c14aaddbc99251ef2d2fd3784
mrcreeppl <38496500+mrcreeppl@users.noreply.github.com>
2018-06-11 08:01:10 +0200

    Update Instrumented Testing
    - adding comments; -executing test on device

--- app/src/androidTest/java/sdp/jakdojde/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/sdp/jakdojde/ExampleInstrumentedTest.java
@@ -10,8 +10,8 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumented test, which will execute on an Android device.
- *
+ * You can execute this Instrumented Test on your Android device.
+ * To gain more information about testing from cmd visit <a href="https://developer.android.com/studio/test/command-line"> Command Line  </a>
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
